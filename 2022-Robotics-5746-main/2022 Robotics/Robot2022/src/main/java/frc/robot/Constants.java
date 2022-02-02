67301122e7c0cf4c8c64bf3da7b8ecdbe95a26f5
Doozie720 <98849032+Doozie720@users.noreply.github.com>
2022-02-02 17:00:46 -0500

    comitting what jobe did in robotics

--- 2022-Robotics-5746-main/2022 Robotics/Robot2022/src/main/java/frc/robot/Constants.java
+++ 2022-Robotics-5746-main/2022 Robotics/Robot2022/src/main/java/frc/robot/Constants.java
@@ -16,5 +16,9 @@ package frc.robot;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
-    double PI = 3.14;
-}
+
+	public static final int MOTOR_LEFT_1_ID = 0;
+	public static final int MOTOR_LEFT_2_ID = 1;
+	public static final int MOTOR_RIGHT_1_ID = 2;
+	public static final int MOTOR_RIGHT_2_ID = 3;
+	public static final int DRIVER_CONTROLLER = 0;}
--- 2022-Robotics-5746-main/2022 Robotics/Robot2022/src/main/java/frc/robot/Robot.java
+++ 2022-Robotics-5746-main/2022 Robotics/Robot2022/src/main/java/frc/robot/Robot.java
@@ -6,7 +6,7 @@
 /*----------------------------------------------------------------------------*/
 
 package frc.robot;
-//WE PUSHIN P
+
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
