e883818ea1a324fb99cf230b1a786191b37633b2
Jobe Goetz <84696795+jobegoetz@users.noreply.github.com>
2022-02-02 16:08:03 -0500

    day 1 left off on 21:31 on FRC Java Tutorial - WPILib 2019 Command Based Framework Ep 2. Drivetrain and Driver Controls

--- 2022 Robotics/Robot2022/src/main/java/frc/robot/RobotContainer.java
+++ 2022 Robotics/Robot2022/src/main/java/frc/robot/RobotContainer.java
@@ -9,8 +9,6 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 
 /**
@@ -21,9 +19,6 @@ import edu.wpi.first.wpilibj2.command.Command;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
-
-  private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
 
 
 
