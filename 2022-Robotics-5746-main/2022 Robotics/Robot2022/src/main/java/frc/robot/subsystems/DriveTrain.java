8a58c0ab23e85afdf5b22a6b28fbdd24dfafe273
Doozie720 <98849032+Doozie720@users.noreply.github.com>
2022-02-02 17:03:23 -0500

    test

--- 2022-Robotics-5746-main/2022 Robotics/Robot2022/src/main/java/frc/robot/subsystems/DriveTrain.java
+++ 2022-Robotics-5746-main/2022 Robotics/Robot2022/src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -9,7 +9,6 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
